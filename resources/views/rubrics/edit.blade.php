8e10c056502b1fb99bdb3066353348fe3524ec58
dura <m.durra@eaa.com.sa>
2019-07-21 13:36:30 +0300

    Message Confirm Delete rubric and fixes design

--- resources/views/rubrics/edit.blade.php
+++ resources/views/rubrics/edit.blade.php
@@ -164,16 +164,5 @@
             })
         }
 
-        // $('input.btnDelete').on('click', function (e) {
-        //     e.preventDefault();
-        //     var id = $(this).closest('tr').data('id');
-        //     $('#myModal').data('id', id).modal('show');
-        // });
-
-        // $('#btnDelteYes').click(function () {
-        //     var id = $('#myModal').data('id');
-        //     $('[data-id=' + id + ']').remove();
-        //     $('#myModal').modal('hide');
-        // });
     </script>
 @endpush
