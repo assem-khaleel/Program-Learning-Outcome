bb064b3262b2f0f6f3636068864689d5fd3cf77e
dura <m.durra@eaa.com.sa>
2019-07-30 12:38:38 +0300

    progress faculty

--- resources/views/dashboardFaculty.blade.php
+++ resources/views/dashboardFaculty.blade.php
@@ -8,8 +8,6 @@
         <div class="col-md-12 align-self-center">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{route('home')}}">{{trans('common.home')}}</a></li>
-                <li class="breadcrumb-item">Widget</li>
-                <li class="breadcrumb-item active">Data</li>
             </ol>
         </div>
     </div>
